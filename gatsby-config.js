fbef91210730843dacaaa0396141840ae243815f
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-09-26 15:31:44 -0400

    Change the gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,15 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     ,
     {
       resolve: `gatsby-plugin-styled-components`,
