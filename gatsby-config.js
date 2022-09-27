1790e70dc5f69de525dac014a094f6525d1829bd
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-09-27 14:15:51 -0400

    added the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,21 +7,10 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: path.join(__dirname, `src`, `images`),
-      },
-    },
+   `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    ,
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
+`gatsby-plugin-styled-components`
+   
   ],
 }
