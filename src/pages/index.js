84e7b2ab360c22a09a6f134784cc01ee308df75b
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-09-28 09:35:38 -0400

    change the image import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import Images from "../examples/images"
 
 export default function Home() {
   return (
