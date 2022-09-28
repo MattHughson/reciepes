14e458bc2d93fd10fa93d6de780db4c52c2c64d0
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-09-28 10:26:15 -0400

    Modifying the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,24 @@
 import React from "react"
 import Layout from "../components/Layout"
-
+import {StaticImage} from 'gatsby-plugin-image'
+import {Link} from 'gatsby'
 const About = () => {
   return (
   <Layout>
-    
+    <main className='page'>
+
+      <section className='about-page'>
+        <article>
+          <h2>I'm a baby cloring boke poke txidery</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque est culpa officiis sit incidunt tempore a porro reprehenderit repudiandae nesciunt. Sed asperiores sequi libero numquam repudiandae inventore deleniti optio aliquam?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis accusamus commodi rem illo, vel facere repellendus laborum accusantium ad architecto a eveniet ducimus ab tempora debitis aspernatur doloribus? Commodi.</p>
+          <Link to='/contact' className='btn'>
+            contact
+          </Link>
+        </article>
+<StaticImage scr="/Users/matt/Documents/projects/Gatsby/reciepes/src/Assets/images/about.jpeg" alt="person"/>
+      </section>
+    </main>
     <h1>About</h1>
     </Layout>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
+
 import Layout from "../components/Layout"
-import Images from "../examples/images"
+
 
 export default function Home() {
   return (
     <Layout>
       <h1>homepage</h1>
-      <Images/>
+  
     </Layout>
   )
 }
