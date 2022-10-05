79aa086a8351efc426c40fd9499a68edac5fc198
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-10-05 15:25:58 -0400

    fixed the nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,29 @@
 import React from "react"
 
 import Layout from "../components/Layout"
+import {StaticImage}  from 'gatsby-plugin-image'
+
 
 
 export default function Home() {
   return (
     <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+          src = '/Users/matt/Documents/projects/Gatsby/reciepes/src/Assets/images/main.jpeg'
+          alt='eggs'
+          className='hero-img'
+          placeholder="tracedSVG"
+          Layout="fullWidth" />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simple recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
       <h1>homepage</h1>
   
     </Layout>
