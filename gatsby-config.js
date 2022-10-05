c0452e4325bedcd4d1dd2ab87a93a0bde50bc259
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-10-05 15:56:52 -0400

    added data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,20 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata:{
+    title:'Simply Recipes',
+    description:'Nice and clean recipes site',
+    author:"@mattHughson",
+    person:{
+      name:'Matt', age:32},
+      simpleData:[
+      'Matt', 32
+      ],
+      complexData:[{
+      name:'Matt', age:32},{
+      name:'Matt', age:37}]
+    
+  }
   plugins: [
    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
