09f9fa543bd8d430866c119bca12e75a8407e2f7
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-10-27 14:55:48 -0400

    added file source system

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`,
+        path: `${__dirname}/src/Assets/images`,
       }},
 ]
 }
