c03356240749eb27cad8ab5d451a8f16bec96bd7
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-10-31 14:24:58 -0400

    addeed the gallery componnenet

---
+++ src/components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Gallery = () => {
+  return (
+    <div>
+      apple
+    </div>
+  )
+}
+
+export default Gallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,15 @@ import React from "react"
 import Layout from "../components/Layout"
 import {StaticImage}  from 'gatsby-plugin-image'
 import FetchData from '../examples/fetchData'
+import Gallery from '../components'
 
 
 
 export default function Home() {
   return (
     <Layout>
-      <main className="page">
-        <header className="hero">
-          <StaticImage
-          src = '../Assets/images/main.jpeg'
-          alt='eggs'
-          className='hero-img'
-          placeholder="tracedSVG"
-          layout="fullWidth"/>
-          <div className="hero-container">
-            <div className="hero-text">
-              <h1>simple recipes</h1>
-              <h4>no fluff, just recipes</h4>
-            </div>
-          </div>
-        </header>
-        <FetchData/>
-      </main>
-      <h1>homepage</h1>
-  
+      <Gallery/>
+
     </Layout>
   )
 }
