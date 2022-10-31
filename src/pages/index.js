136dd283827eb31aeb12b5fc2841a6c4982dc22c
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-10-31 14:41:29 -0400

    cleaned up the code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react"
 
 import Layout from "../components/Layout"
-import {StaticImage}  from 'gatsby-plugin-image'
-import FetchData from '../examples/fetchData'
+
 import Gallery from '../components/Gallery'
 
 
