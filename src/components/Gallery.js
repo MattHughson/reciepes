0d965f3df985c0e2f80f362916726fa36a3fabb5
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-11-15 10:37:28 -0500

    added image gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,44 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+import {GatsbyImage} from 'gatsby-plugin-image'
 
+
+const query = graphql`
+  {
+    allFile(filter: {extension: {ne: "svg"}}) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            transformOptions: {grayscale: true}
+            width:200
+          )
+        }
+      }
+    }
+  }
+`
 
 const Gallery = () => {
+  const data = useStaticQuery(query)
+const nodes = data.allFile.nodes
   return (
-    <div>
-      apple
-    </div>
+    <Wrapper>
+      {nodes.map((image,idex) => {
+        const {name} = image
+        return(
+          <article key={index}>
+            <GatsbyImage image={image.childImageSharp.gatsbyImageData} />
+          </article>
+        )
+      })}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+`
 
 export default Gallery
