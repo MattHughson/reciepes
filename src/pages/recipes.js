bb21084c63f785e33588b0efd4eacae6e361d888
Matthew Hughson <matthew.hughson@lesappelsillimites.com>
2022-11-18 13:28:00 -0500

    added the reicpe list componnent

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -3,6 +3,7 @@ import AllReceipes from "../components/AllReceipes"
 import Layout from "../components/Layout"
 import {Link} from 'gatsby'
 import {GatsbyImage} from "gatsby-plugin-image"
+import ReciepeList from "../components/ReciepeList"
 
 const Recipes = () => {
   return (
@@ -10,6 +11,8 @@ const Recipes = () => {
     
     <h1>Recipes</h1>
     <AllReceipes/>
+    <ReciepeList/>
+    
 
     </Layout>
     )
